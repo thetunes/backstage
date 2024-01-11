1f80ec443a98b3b3519de727d55607fa6ac3f9c5
HeroBuxx <me@herobuxx.me>
2024-01-11 19:38:24 +0700

    backstage: Redirect unathorized user into backdoor
    Signed-off-by: HeroBuxx <me@herobuxx.me>

--- src/js/guard.js
+++ src/js/guard.js
@@ -3,5 +3,5 @@ console.log("guard.js script loaded");
 import { token } from "./controller/cookies.js";
 
 if (!token) {
-  window.location.replace("http://tunes.herobuxx.me/gate/");
+  window.location.replace("http://tunes.herobuxx.me/backdoor/");
 }
